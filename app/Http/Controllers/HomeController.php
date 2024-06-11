3e52aa1e4d625071c00cda0809611de124f9008c
Sezer5 <unalmissezer@gmail.com>
2024-06-12 00:22:01 +0300

    User Template Added

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        //echo "Index Function!";
+        return view('home.index');
+    }
+
+    public function test()
+    {
+        //
+        return view('home.test');
+    }
+
+    public function param($id,$id2)
+    {
+        //
+        //echo "parameter 1 is:",$id;
+        //echo "<br>";
+        //echo "parameter 2 is:",$id2;
+        return view('home.test2',[
+            'id'=>$id,
+            'id2'=>$id2
+        ]);
+    }
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,35 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\HomeController as HomeController;
 
-Route::get('/', function () {
+// 1-Write a message with route
+
+Route::get('/hello', function () {
+    return 'Hello World!';
+});
+
+// 2-Call view in the route
+
+Route::get('/sampleview', function () {
     return view('welcome');
 });
+
+// 3-Call the controller function
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// 4- Route->Controller->View
+
+Route::get('/test', [HomeController::class, 'test'])->name('test');
+
+// 5-Route with parameter
+
+Route::get('/param/{id}/{id2}', [HomeController::class, 'param'])->name('param');
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::middleware([
     'auth:sanctum',
