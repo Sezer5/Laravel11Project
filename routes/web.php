9ee25e86f27ed89bb22b2751b22e2213fb7a63f9
Sezer5 <unalmissezer@gmail.com>
2024-06-12 23:54:55 +0300

    Category Models And Controller Added

--- routes/web.php
+++ routes/web.php
@@ -3,30 +3,45 @@
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\HomeController as HomeController;
 use \App\Http\Controllers\Admin\HomeController as AdminHomeController;
+use \App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
 
-// 1-Write a message with route
+// // 1-Write a message with route
 
-Route::get('/hello', function () {
-    return 'Hello World!';
-});
+// Route::get('/hello', function () {
+//     return 'Hello World!';
+// });
 
-// 2-Call view in the route
+// // 2-Call view in the route
 
-Route::get('/sampleview', function () {
-    return view('welcome');
-});
+// Route::get('/sampleview', function () {
+//     return view('welcome');
+// });
 
-// 3-Call the controller function
+// // 3-Call the controller function
+
+// *****************************USER ROUTES ********************************
+// *****************************USER ROUTES ********************************
+// *****************************USER ROUTES ********************************
+// *****************************USER ROUTES ********************************
+// *****************************USER ROUTES ********************************
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// *****************************ADMIN ROUTES *******************************
+// *****************************ADMIN ROUTES *******************************
+// *****************************ADMIN ROUTES *******************************
+// *****************************ADMIN ROUTES *******************************
+// *****************************ADMIN ROUTES *******************************
 Route::get('/admin', [AdminHomeController::class, 'index'])->name('admin');
+Route::get('/admin/category', [AdminCategoryController::class, 'index'])->name('admin_category');
+Route::get('/admin/category/create', [AdminCategoryController::class, 'create'])->name('admin_category_create');
 
-// 4- Route->Controller->View
+// // 4- Route->Controller->View
 
-Route::get('/test', [HomeController::class, 'test'])->name('test');
+// Route::get('/test', [HomeController::class, 'test'])->name('test');
 
-// 5-Route with parameter
+// // 5-Route with parameter
 
-Route::get('/param/{id}/{id2}', [HomeController::class, 'param'])->name('param');
+// Route::get('/param/{id}/{id2}', [HomeController::class, 'param'])->name('param');
 
 
 // Route::get('/', function () {
